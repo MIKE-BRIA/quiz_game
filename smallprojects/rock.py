4586caf14a7617cfdf1663ee821a78cd1f8b6533
Brian Michael <brianmichaeladero@gmail.com>
2023-05-27 22:07:14 +0300

    rockpaperscissors

--- smallprojects/rock.py
+++ smallprojects/rock.py
@@ -1,52 +1,30 @@
-print("welcome to the life game challenge")
+from getpass import getpass as input
 
-playing = input("Are you sure you wanna play? ")
-if playing.lower()!= "yes":
-    quit()
+print("Player one")
+p1=input("Choose between rock paper and scissors: ")
+print("Player two")
+p2=input('Choose between rock, paper and scissors: ')
 
-print("okay lets play")
-score=0
-answer= input("what is the name of kenya capital city? ").lower()
-if answer == "nairobi":
-    print('correct!')
-    score +=1
+if p1== "rock" :
+  if p2 == "scissors":
+    print("Player one won the game")
+  elif p2 == "paper":
+    print("Player two won the game")
+  elif p2 == "rock":
+    print("Both player one and two tied")
+elif p1 == "paper" :
+  if p2 == "rock":
+    print("Player two won the game")
+  elif p2 == "paper":
+    print("Both player one and two tied")
+  elif p2 == "scissors":
+    print("Player two won the game")
+elif p1 == "scissors":
+  if p2 == "rock":
+    print("Player two won the game")
+  elif p2 =="paper":
+    print("Player one won the game")
+  elif p2 =="scissors":
+    print("Both player one and two tied")
 else:
-    print('incorrect!')
-
-answer = input("where is the main port situated in Kenya? ").lower()
-if answer == "mombasa":
-    print('correct!')
-    score +=1
-else:
-    print('incorrect!')
-
-answer = input("are kenyan ladies good at giving guys character development? ").lower()
-if answer == "yes ":
-    print('correct!')
-    score +=1
-else:
-    print('incorrect!')
-
-answer = input("what is the most toxic county? ").lower()
-if answer == "garrisa":
-    print('correct!')
-    score +=1
-else:
-    print('incorrect!')
-
-answer = input("which people consume a lot of fish? ").lower()
-if answer == "luos":
-    print('correct!')
-    score +=1
-else:
-    print('incorrect!')
-
-answer = input("do you want to have a kenyan girlfriend when you are still broke? ").lower()
-if answer == "no":
-    print('correct!')
-    score +=1
-else:
-    print('incorrect!')
-    
-print("you got" + str(score) + "questions correct")
-print("you got" + str((score/6)*100)+"%")
+  print("are you sure you placed the correct inputs")
