ed5f3cb6c1199997ac5a79d3bdc9a8376cd8e04b
Brian Michael <brianmichaeladero@gmail.com>
2023-05-29 14:27:44 +0300

    for loop in range with inputs

---
+++ 100_days_of_code/day20.py
@@ -0,0 +1,8 @@
+print("WE ARE GOING TO WORK WITH NUMBERS")
+
+doo=int(input("Which number do you want to start with? "))
+loo=int(input("Which number do you want to end with? "))
+boo=int(input("In what intervals do you want your numbers to be in? "))
+
+for i in range(doo,loo,boo):
+    print(i)
