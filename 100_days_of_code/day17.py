51c4f0d26f8f183018c451b0a1edc9d4555d2df1
Brian Michael <brianmichaeladero@gmail.com>
2023-05-29 11:08:56 +0300

    exit,continue and break function

--- 100_days_of_code/day17.py
+++ 100_days_of_code/day17.py
@@ -1,30 +1,50 @@
 from getpass import getpass as input
 
-print("Player one")
-p1=input("Choose between rock paper and scissors: ")
-print("Player two")
-p2=input('Choose between rock, paper and scissors: ')
+player1score=0
+player2score=0
 
-if p1== "rock" :
-  if p2 == "scissors":
-    print("Player one won the game")
-  elif p2 == "paper":
-    print("Player two won the game")
-  elif p2 == "rock":
-    print("Both player one and two tied")
-elif p1 == "paper" :
-  if p2 == "rock":
-    print("Player two won the game")
-  elif p2 == "paper":
-    print("Both player one and two tied")
-  elif p2 == "scissors":
-    print("Player two won the game")
-elif p1 == "scissors":
-  if p2 == "rock":
-    print("Player two won the game")
-  elif p2 =="paper":
-    print("Player one won the game")
-  elif p2 =="scissors":
-    print("Both player one and two tied")
-else:
-  print("are you sure you placed the correct inputs")
+while True:
+  print("Player one")
+  p1=input("Choose between rock paper and scissors: ")
+  print("Player two")
+  p2=input('Choose between rock, paper and scissors: ')
+
+  if p1== "rock" :
+    if p2 == "scissors":
+      print("Player one won the game")
+      player1score+=1
+    elif p2 == "paper":
+      print("Player two won the game")
+      player2score+=1
+    elif p2 == "rock":
+      print("Both player one and two tied")
+  elif p1 == "paper" :
+    if p2 == "rock":
+      print("Player two won the game")
+      player2score+=1
+    elif p2 == "paper":
+      print("Both player one and two tied")
+    elif p2 == "scissors":
+      print("Player two won the game")
+      player2score+=1
+  elif p1 == "scissors":
+    if p2 == "rock":
+      print("Player two won the game")
+      player2score+=1
+    elif p2 =="paper":
+      print("Player one won the game")
+      player1score+=1
+    elif p2 =="scissors":
+      print("Both player one and two tied")
+  else:
+    print("are you sure you placed the correct inputs")
+    
+    
+  print("player one has",player1score,"points")
+  print("player two has",player2score,"points")
+    
+  if player1score==5 or player2score==5:
+    print("We welcome you next time for a better experience")
+    exit()
+  else:
+    continue
