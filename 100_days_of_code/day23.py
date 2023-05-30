70158eb523518bf6616213f91bcf795b8bba864f
Brian Michael <brianmichaeladero@gmail.com>
2023-05-30 21:58:45 +0300

    function

---
+++ 100_days_of_code/day23.py
@@ -0,0 +1,13 @@
+def login():
+   while True:
+       username=input("Enter your username: ")
+       password=input("Enter your password: ")
+       if username == "Brian" and password == "Bria001":
+           print("Welcome to BIMAX Group of companies")
+           break
+       else:
+           print("check your username and password")
+           
+           
+print("BIMAX GROUP OF CAMPANIES")
+login()
