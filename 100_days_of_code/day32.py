afed2eb3b8c27024adf6fab9a74479f91049a1a6
Brian Michael <brianmichaeladero@gmail.com>
2023-06-03 13:11:28 +0300

    list aand loops random

---
+++ 100_days_of_code/day32.py
@@ -0,0 +1,8 @@
+import random
+
+greeting=['Bonjour','Hola','Zdravstvuyte','Nǐn hǎo','Salve','Konnichiwa','Guten Tag',
+          'Olá','Anyoung haseyo','Asalaam alaikum','Goddag','Shikamoo','Goedendag','Yassas',
+          'Dzień dobry','Selamat siang','Namaste, Namaskar','God dag','Merhaba','Shalom','God dag']
+
+index =random.randint(0,22)
+print(greeting[index])
