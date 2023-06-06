6100cbd82f9f8cefc6a0e2fcb7a3b379ec88b61f
Brian Michael <brianmichaeladero@gmail.com>
2023-06-06 12:57:23 +0300

    no duplicates

---
+++ 100_days_of_code/day36.py
@@ -0,0 +1,18 @@
+names=[]
+
+def pring():
+    print()
+    for i in names:
+        print(i)
+    print()
+
+while True:
+    first=input("What's your first name: ").strip().capitalize()
+    last=input("What's your last name: ").strip().capitalize()
+    name=f"{first} {last}"
+    
+    if name not in names:
+        names.append(name)
+    else:
+        print("That is already present")
+    pring
