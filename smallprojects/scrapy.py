e67de24bff37f37e06af955651533bd16843dab3
Brian Michael <brianmichaeladero@gmail.com>
2023-06-08 21:01:50 +0300

    projects

---
+++ smallprojects/scrapy.py
@@ -0,0 +1,14 @@
+names=[]
+
+def pring():
+    for i in list:
+        print(i)
+
+
+while True:
+    first=input("What's your first name: ")
+    last=input("What's your last name: ")
+    if first and last not in names:
+        names.append(first and last)
+    pring()
+    
