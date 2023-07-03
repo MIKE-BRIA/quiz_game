9f10c369cbc5491a6796303e256c7edad40aac3f
Brian Michael <brianmichaeladero@gmail.com>
2023-07-03 16:56:08 +0300

    updated

---
+++ dailycoder/fun.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle = turtle.Turtle()
+
+def square():
+  turtle.forward(80)
+  turtle.left(90)
+  turtle.forward(80)
+  turtle.left(90)
+  turtle.forward(80)
+  turtle.left(90)
+  turtle.forward(80)
+while True:
+  square()
+  turtle.forward(90)
+  square()
+  turtle.forward(90)
+  square()
+  turtle.forward(90)
+  square()
+  turtle.forward(90)
+  square()
